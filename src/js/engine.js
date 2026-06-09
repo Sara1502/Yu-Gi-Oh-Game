da64b97a2fe60188b142334206e07a302de72a20
Sara1502 <sara.moreira1520@gmail.com>
2026-06-09 07:32:23 -0300

    Lógica de tirar outras cartas quando uma esta selecionada

--- src/js/engine.js
+++ src/js/engine.js
@@ -13,15 +13,18 @@ const state = {
         player: document.getElementById('player-field-card'),
         computer: document.getElementById('computer-field-card'),
     },
+    playerSides: {
+        player1: 'player-cards',
+        player1BOX: document.querySelector('#player-cards'),
+        computer: 'computer-cards',
+        computerBOX: document.querySelector('#computer-cards'),
+    },
     actions: {
         button: document.getElementById('next-duel'),
     },
 };
 
-const playerSides = {
-    player1: 'player-cards',
-    computer: 'computer-cards',
-}
+
 
 const pathImage = './src/assets/icons/';
 
@@ -64,17 +67,43 @@ async function creatCardImage(IdCard, fieldSide) {
     cardImage.setAttribute('data-id', IdCard);
     cardImage.classList.add('card');
 
-    if (fieldSide === playerSides.player1) {
+    if (fieldSide === state.playerSides.player1) {
+        cardImage.addEventListener('mouseover', () => {
+            drawSelectedCard(IdCard)
+        })
+
         cardImage.addEventListener('click', () => {
-            setCardsField(cardImage.getAttribute('data-id'));
+            setCardField(cardImage.getAttribute('data-id'));
         });
     }
 
-    cardImage.addEventListener('mouseover', () => {
-        drawSelectedCard(IdCard)
-    })
-
     return cardImage
+}
+
+async function setCardField(cardId) {
+    await removeAllCardsImages();
+
+    let computerCardId = await getRandomCardId();
+
+    state.fieldCards.player.style.display = 'block';
+    state.fieldCards.computer.style.display = 'block';
+
+    state.fieldCards.player.src = cardData[cardId].img;
+    state.fieldCards.computer.src = cardData[computerCardId].img;
+
+    let duelResuts = await checkDuelResults(cardId, computerCardId);
+
+    await updateScore();
+    await drawButton(duelResuts);
+}
+
+async function removeAllCardsImages() {
+    let { computerBOX, player1BOX } = state.playerSides;
+    let imgElements = computerBOX.querySelectorAll('img');
+    imgElements.forEach((img) => img.remove());
+
+    imgElements = player1BOX.querySelectorAll('img');
+    imgElements.forEach((img) => img.remove());
 }
 
 async function drawSelectedCard(index) {
@@ -95,8 +124,8 @@ async function drawCards(cardNumber, fieldSide) {
 
 
 function init() {
-    drawCards(5, playerSides.player1);
-    drawCards(5, playerSides.computer);
+    drawCards(5, state.playerSides.player1);
+    drawCards(5, state.playerSides.computer);
 }
 
 init();
